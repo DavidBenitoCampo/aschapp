0ce22b79484e101f1d7c82f7b93cae4f1c62d894
DavidBenitoCampo <davbecam@gmail.com>
2021-03-04 17:46:23 +0100

    Creación de los componentes login, formulario, feed, contenido, compra, carrito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { LoginComponent } from './components/login/login.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    CompraComponent,
+    LoginComponent,
+    CarritoComponent,
+    FeedComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
