632cf8d484f3036aeecccce21e4b5d67faaf7fab
Cristian Gomez <crisstianc@gmail.com>
2021-03-17 09:32:27 +0100

    añadir routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'
+import { FeedComponent } from './components/feed/feed.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { RegistroProductoComponent } from './components/registro-producto/registro-producto.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
-
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: FeedComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: FormularioComponent },
+  { path: 'addProducto', component: RegistroProductoComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: '**', redirectTo: '/home' }
+]
 @NgModule({
   declarations: [],
   imports: [
